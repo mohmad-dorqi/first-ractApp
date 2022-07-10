0f8a6b946a4f5d060cc76435435f5967162c7c83
mohmad-dorqi <mohmad.dorqi4748@gmail.com>
2022-07-10 11:09:50 +0430

    new component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,17 @@ import React ,{Component} from "react";
 import Navbar from "./components/Navbar";
 import Baner from "./components/Baner";
 import Cards from "./components/Cards";
+import Search from "./components/Search";
+import Logo from "./components/Logo";
 class App extends Component {
   render(){
     return (
       <div>
        <Navbar /> 
        <Baner />
-      <Cards />
+       <Cards />
+       <Search />
+       <Logo />
       </div>
     );
   }
